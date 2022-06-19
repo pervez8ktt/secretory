452ea16758ee1f99182cd16a771a0a566578ea39
Pervez Quadri <pervez8ktt@gmail.com>
2022-06-19 19:16:16 +0530

    if config null then redirect to config

--- src/component/Dashboard.js
+++ src/component/Dashboard.js
@@ -1,8 +1,21 @@
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import useConfiguration from "../data/useConfiguration"
 import Head from "./head/Head"
 
 const Dashboard = (props) => {
 
-    
+    const {getList: getConfigList}= useConfiguration();
+
+    const navigation = useNavigate();
+
+    useEffect(()=>{
+        getConfigList((response)=>{
+            if(response==null){
+                navigation("configuration")
+            }
+        });
+    })
 
     return <>
         <Head title="Dashboard" />
